12aaa3a241aaf26436c28ec0f3016947b8b81443
Seng Yew Chua <sengyew07@gmail.com>
2023-11-17 22:37:39 +0800

    add image to itinerary

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # alf1650-6m-software-5-projectwork
+
 6m-software-5-projectwork
 
 npx expo install expo-calendar
 npx expo install @react-native-community/datetimepicker
+npx expo install expo-image-picker
--- project-app/App.js
+++ project-app/App.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   View,
   Button,
+  Image,
+  StyleSheet,
   Text,
   TextInput,
   Platform,
@@ -9,30 +11,34 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Alert,
-} from 'react-native';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import * as Calendar from 'expo-calendar';
+} from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import * as Calendar from "expo-calendar";
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
   const [calendarId, setCalendarId] = useState(null);
   const [showStartDatePicker, setShowStartDatePicker] = useState(false);
   const [showEndDatePicker, setShowEndDatePicker] = useState(false);
   const [tripAdded, setTripAdded] = useState(false);
+  const [image, setImage] = useState(null);
   const [tripDetails, setTripDetails] = useState({
-    title: 'My Trip',
+    title: "My Trip",
     startDate: new Date(),
     endDate: new Date(),
     allDay: true, // Set allDay to true for an all-day event
-    location: 'Destination City',
-    description: 'Trip description',
-    timeZone: 'America/New_York',
+    location: "Destination City",
+    description: "Trip description",
+    timeZone: "America/New_York",
   });
 
   useEffect(() => {
     (async () => {
       const { status } = await Calendar.requestCalendarPermissionsAsync();
-      if (status === 'granted') {
-        const calendars = await Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT);
+      if (status === "granted") {
+        const calendars = await Calendar.getCalendarsAsync(
+          Calendar.EntityTypes.EVENT
+        );
         setCalendarId(calendars[0].id);
       }
     })();
@@ -47,7 +53,7 @@ export default function App() {
   };
 
   const handleStartDateChange = (event, selectedDate) => {
-    setShowStartDatePicker(Platform.OS === 'ios');
+    setShowStartDatePicker(Platform.OS === "ios");
     setTripDetails({
       ...tripDetails,
       startDate: selectedDate || tripDetails.startDate,
@@ -55,7 +61,7 @@ export default function App() {
   };
 
   const handleEndDateChange = (event, selectedDate) => {
-    setShowEndDatePicker(Platform.OS === 'ios');
+    setShowEndDatePicker(Platform.OS === "ios");
     setTripDetails({
       ...tripDetails,
       endDate: selectedDate || tripDetails.endDate,
@@ -83,9 +89,25 @@ export default function App() {
     });
   };
 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   const addTripToCalendar = async () => {
     if (!calendarId) {
-      console.error('Calendar not found');
+      console.error("Calendar not found");
       return;
     }
 
@@ -97,67 +119,141 @@ export default function App() {
         setTripAdded(false);
       }, 3000); // Reset the "Added to Calendar" message after 3 seconds
     } catch (error) {
-      console.error('Error adding event to calendar:', error);
-      Alert.alert('Error', 'Failed to add event to calendar.');
+      console.error("Error adding event to calendar:", error);
+      Alert.alert("Error", "Failed to add event to calendar.");
     }
   };
 
   return (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <KeyboardAvoidingView
-        style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      >
-        <Text>Start Date: {tripDetails.startDate.toDateString()}</Text>
-        <Button title="Select Start Date" onPress={showStartDatePickerModal} />
-
-        <Text>End Date: {tripDetails.endDate.toDateString()}</Text>
-        <Button title="Select End Date" onPress={showEndDatePickerModal} />
-
-        <Text>Title:</Text>
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1, margin: 10, padding: 5 }}
-          onChangeText={handleTitleChange}
-          value={tripDetails.title}
-        />
-
-        <Text>Location:</Text>
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1, margin: 10, padding: 5 }}
-          onChangeText={handleLocationChange}
-          value={tripDetails.location}
-        />
-
-        <Text>Description:</Text>
-        <TextInput
-          style={{ height: 80, borderColor: 'gray', borderWidth: 1, margin: 10, padding: 5 }}
-          multiline
-          onChangeText={handleDescriptionChange}
-          value={tripDetails.description}
-        />
-
-        {showStartDatePicker && (
-          <DateTimePicker
-            value={tripDetails.startDate}
-            mode="date"
-            display="default"
-            onChange={handleStartDateChange}
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <KeyboardAvoidingView
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+          <Text style={styles.text}>
+            Start Date: {tripDetails.startDate.toDateString()}
+          </Text>
+          <Button
+            style={styles.button}
+            title="Select Start Date"
+            onPress={showStartDatePickerModal}
           />
-        )}
 
-        {showEndDatePicker && (
-          <DateTimePicker
-            value={tripDetails.endDate}
-            mode="date"
-            display="default"
-            onChange={handleEndDateChange}
+          <Text style={styles.text}>
+            End Date: {tripDetails.endDate.toDateString()}
+          </Text>
+          <Button
+            style={styles.button}
+            title="Select End Date"
+            onPress={showEndDatePickerModal}
           />
-        )}
 
-        <Button title="Add Trip to Calendar" onPress={addTripToCalendar} />
+          <Text style={styles.text}>Title:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={handleTitleChange}
+            value={tripDetails.title}
+          />
 
-        {tripAdded && <Text style={{ color: 'green' }}>Added to Calendar</Text>}
-      </KeyboardAvoidingView>
-    </TouchableWithoutFeedback>
+          <Text style={styles.text}>Location:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={handleLocationChange}
+            value={tripDetails.location}
+          />
+
+          <Text style={styles.text}>Description:</Text>
+          <TextInput
+            style={styles.textarea}
+            multiline
+            onChangeText={handleDescriptionChange}
+            value={tripDetails.description}
+          />
+          <Button
+            style={styles.button}
+            title="Add Image for Itinerary"
+            onPress={pickImage}
+          />
+          {image && <Image source={{ uri: image }} style={styles.image} />}
+
+          {showStartDatePicker && (
+            <DateTimePicker
+              value={tripDetails.startDate}
+              mode="date"
+              display="default"
+              onChange={handleStartDateChange}
+            />
+          )}
+
+          {showEndDatePicker && (
+            <DateTimePicker
+              value={tripDetails.endDate}
+              mode="date"
+              display="default"
+              onChange={handleEndDateChange}
+            />
+          )}
+
+          <Button
+            style={styles.button}
+            title="Add Trip to Calendar"
+            onPress={addTripToCalendar}
+          />
+
+          {tripAdded && (
+            <Text style={styles.successText}>Added to Calendar</Text>
+          )}
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5", // Set your desired background color
+    paddingHorizontal: 20,
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 5,
+    textAlign: "center",
+  },
+  button: {
+    marginVertical: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginVertical: 10,
+    padding: 10,
+    borderRadius: 5,
+    width: "100%",
+  },
+  textarea: {
+    height: 80,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginVertical: 10,
+    padding: 10,
+    borderRadius: 5,
+    width: "100%",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: "cover",
+    marginVertical: 20,
+    borderRadius: 10,
+  },
+  successText: {
+    color: "green",
+    textAlign: "center",
+    marginTop: 10,
+  },
+});
